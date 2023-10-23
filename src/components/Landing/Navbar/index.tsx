f5a70375e3f11a4f3ac89cd5551b2d83af0669ca
Godswill Adigwe <gcadigwe@gmail.com>
2023-10-23 15:33:47 +0100

    navbar

--- src/components/Landing/Navbar/index.tsx
+++ src/components/Landing/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { Button, Flex, Img, Text, useMediaQuery } from "@chakra-ui/react";
 import hamburger from "../../../assets/hamburger.svg";
+import Link from "next/link";
 
 export default function Navbar() {
   const [isMobileDevice] = useMediaQuery("(max-width: 559px)");
@@ -20,10 +21,19 @@ export default function Navbar() {
             fontFamily={"manropebd"}
             fontSize={{ lg: "18px", xl: "22px" }}
           >
-            <Text>Governance</Text>
-            <Text>Documentation</Text>
-            <Text>Lending</Text>
-            <Text>Yield</Text>
+            <Link href='/daov2'>
+              <Text>Governance</Text>
+            </Link>
+            <Link href='/lsd'>
+              <Text>LSD</Text>
+            </Link>
+            <Link href='/lending'>
+              <Text>Lending</Text>
+            </Link>
+
+            <Link href='/yieldv2'>
+              <Text>Yield</Text>
+            </Link>
             <Button borderRadius={"18px"} bgColor={"rgba(45, 47, 80, 1)"}>
               Launch App
             </Button>
